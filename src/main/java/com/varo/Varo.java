75b0ad73e124a6730f4ca2be58da877a9054b55f
Daniel <dethrimlp@gmail.com>
2021-07-10 14:12:54 +0200

    added some code in main class

--- src/main/java/com/varo/Varo.java
+++ src/main/java/com/varo/Varo.java
@@ -1,4 +1,28 @@
 package com.varo;
 
-public class Varo {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Varo extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Plugin Varo started");
+
+        registerCommands();
+        registerListener();
+    }
+
+
+    private void registerCommands() {
+
+    }
+
+    private void registerListener() {
+
+    }
+
 }
